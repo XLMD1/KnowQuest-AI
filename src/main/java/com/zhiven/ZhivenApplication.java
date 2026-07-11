2587dd8aea315d1557e151376bbef5480760e762
XLMD1 <3150527756@qq.com>
2026-07-11 22:36:36 +0800

    chore: 初始化项目骨架

---
+++ src/main/java/com/zhiven/ZhivenApplication.java
@@ -0,0 +1,11 @@
+package com.zhiven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhivenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZhivenApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zhiven/ZhivenApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zhiven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ZhivenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
